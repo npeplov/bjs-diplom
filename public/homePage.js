a4982d012ae27704a8b55aa7c92dcf3fdc47d87b
Nikita Peplov <npeplov@yandex.ru>
2020-09-20 15:22:35 +0300

    Added success messages / Fixed sentInterval(): func()->func / Cleaned code & comments / Added sendMoneyAction() to MoneyManager obj.

--- public/homePage.js
+++ public/homePage.js
@@ -1,91 +1,87 @@
-// 1. Создайте объект класса LogoutButton
 const logout = new LogoutButton;
 
-// 2. В свойство action запишите функцию, которая будет вызывать запрос деавторизации (logout)
 logout.action = () => ApiConnector.logout( (response) => {
-// 3. В колбек запроса добавьте проверку: если запрос выполнился успешно, то 
-// обновите страницу (с помощью location.reload();
-    if (response.success === true) 
+    if (response.success) 
         location.reload();
 } );
 
-// Выполните запрос на получение текущего пользователя (current),
-ApiConnector.current(
-// в колбеке которого проверьте ответ:   
-    (response) => {
-        // если ответ успешный, то
-        if (response.success === true)
-        // вызовите метод отображения данных профиля (ProfileWidget.showProfile)
-        ProfileWidget.showProfile(
-            // в который передавайте данные ответа от сервера.
-            response.data
-        )
+ApiConnector.current( (response) => {
+        if (response.success)
+        ProfileWidget.showProfile(response.data)
     }
 )
 
-// 1. Создайте объект типа RatesBoard.
 rates = new RatesBoard;
-// 2. Напишите функцию, которая будет 
+
 function showRates() {
-    // выполнять запрос получения курсов валют.
-    ApiConnector.getStocks(response => {
-        // В случае успешного запроса, 
-        if (response.success === true) {
-            // очищайте таблицу с данными (clearTable)
+    ApiConnector.getStocks( (response) => {
+        if (response.success) {
             rates.clearTable()
-            // и заполняйте её (fillTable) полученными данными
             rates.fillTable(response.data)
             console.log("Курсы валют загружены");
         }
     })
 }
 
-// Напишите интервал, который будет многократно выполняться (раз в минуту) и вызывать вашу функцию с получением валют.
-let timerId = setInterval(showRates(), 1000*60)
+showRates()
+setInterval(showRates, 1000*60)
 
-// Создайте объект типа MoneyManager
 let money = new MoneyManager;
 
-// Реализуйте пополнение баланса:
-// 1. Запишите в свойство addMoneyCallback функцию, которая будет выполнять запрос.
 money.addMoneyCallback = (data) => {
-    // Используйте аргумент функции свойства addMoneyCallback для передачи данных data в запрос.
-    ApiConnector.addMoney(data, response => {
-        // В случае успешного запроса
-        if (response.success === true) {
-            // отобразите в профиле новые данные о пользователе из данных ответа от сервера (showProfile).
-            ProfileWidget.showProfile(
-                response.data
-            )
+    ApiConnector.addMoney( data, (response) => {
+        console.log(data, response);
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            money.setMessage(response.success, `Счёт пополнен на ${data.amount} ${data.currency}`);
         }
-    })
+        else
+            money.setMessage(response.success, response.error)
+    } )
 }
+
 money.conversionMoneyCallback = (data) => {
-    ApiConnector.convertMoney(data, response => {
-        if (response.success === true) {
-            ProfileWidget.showProfile(
-                response.data
-            )
+    ApiConnector.convertMoney( data, (response) => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            money.setMessage(response.success, `${data.fromAmount} ${data.fromCurrency} сконвертированы на ${data.targetCurrency}`);
+        }
+        else {
+            money.setMessage(response.success, response.error);
+        }
+    } )
+}
+
+money.sendMoneyCallback = (data) => {
+    ApiConnector.transferMoney( data, (response) => {
+        console.log(data, response);
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            money.setMessage(response.success, `${data.amount} ${data.currency} переведен юзеру №${data.to}`);
+        }
+        else {
+            money.setMessage(response.success, response.error);
         }
     })
 }
 
 let favorites = new FavoritesWidget;
 
-ApiConnector.getFavorites(response => {
-    if (response.success === true) {
+ApiConnector.getFavorites( (response) => {
+    if (response.success) {
         favorites.clearTable();
         favorites.fillTable(response.data);
-        money.updateUsersList(response.data)
+        money.updateUsersList(response.data);
     }
 })
 
 favorites.addUserCallback = (data) => {
-    ApiConnector.addUserToFavorites(data, response => {
-        if (response.success === true) {
+    ApiConnector.addUserToFavorites(data, (response) => {
+        if (response.success) {
             favorites.clearTable();
             favorites.fillTable(response.data);
-            money.updateUsersList(response.data)
+            money.updateUsersList(response.data);
+            favorites.setMessage(response.success, `Товарищ ${data.name} добавлен`)
         }
         else
             favorites.setMessage(response.success, response.error)
@@ -93,11 +89,14 @@ favorites.addUserCallback = (data) => {
 }
 
 favorites.removeUserCallback = (data) => {
-    ApiConnector.removeUserFromFavorites(data, response => {
-        if (response.success === true) {
+    ApiConnector.removeUserFromFavorites(data, (response) => {
+        if (response.success) {
             favorites.clearTable();
             favorites.fillTable(response.data);
-            money.updateUsersList(response.data)
+            money.updateUsersList(response.data);
+            favorites.setMessage(response.success, `Гражданин №${data} удален`)
         }
+        else
+            favorites.setMessage(response.success, response.error)
     })
 }
--- public/loginPage.js
+++ public/loginPage.js
@@ -1,28 +1,18 @@
 "use strict"
-
-// 2. Создайте объект класса UserForm
 const user = new UserForm();
 
-// 3. Присвойте свойству loginFormCallback значение функции
-
-// 3.1 Функция должна выполнять запрос на сервер для попытки авторизации пользователя
 user.loginFormCallback = data => ApiConnector.login(data, response => {
     console.log(response); 
-    // 3.5 В случае успеха запроса обновите страницу (с помощью location.reload();
-    if (response.success === true) 
+    if (response.success) 
         location.reload();
-    // 3.6 В случае провала запроса выведите ошибку в окно для ошибок.
     else
         user.setLoginErrorMessage(response.error);
 });
 
-// ApiConnector.logout(() => 1) 
-
 user.registerFormCallback = data => ApiConnector.register(data, response => {
     console.log(response);
-    if (response.success === true) 
+    if (response.success) 
         location.reload();
-    // 3.6 В случае провала запроса выведите ошибку в окно для ошибок.
     else
         user.setRegisterErrorMessage(response.error);
 })
